3b708825ba2560b5959bd4f78e65c360319226af
NurFirdausR <nurfirdaus.5000@gmail.com>
2021-10-18 13:34:38 +0700

    article

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -129,24 +129,28 @@
             <div class="container" data-aos="fade-up">
                 <div class="d-flex justify-content-center row flex-wrap" data-aos="zoom-in" data-aos-delay="100">
                     @forelse ($article as $artikel)
-                        <div class="col-lg-4 mb-4">
-                            <div class="course-item shadow">
-                                <img src="{{ asset('storage/' . $artikel->image) }}" class="img-fluid" alt="..."
-                                    style="width:100%;height:250px;" />
-                                <div class="course-content">
-                                    <div class="d-flex justify-content-between mb-3">
-                                        <h4>
-                                            <a href="/category/{{ $artikel->category->slug }}"
-                                                class="artikel">{{ $artikel->category->name }}</a>
-                                        </h4>
-                                        <!-- <p class="price">$169</p> -->
-                                    </div>
-                                    <h3><a href="/{{ $artikel->slug }}">{{ $artikel->title }}</a>
-                                    </h3>
-
-                                    <p>{{ $artikel->excerpt }}</p>
-                                    <p><a href="/showartikel/{{ $artikel->slug }}">Read More ...</a></p>
+                    <div class="col-lg-4 mb-4">
+                        <div class="course-item shadow">
+                            @if (file_exists(public_path('article-img/'.$artikel->image)))
+                            <img src="{{ asset('article-img/' . $artikel->image) }}" class="img-fluid" alt="..."
+                            style="width:100%;height:250px;" />
+                            @else 
+                            <img src="{{ asset('storage/' . $artikel->image) }}" class="img-fluid" alt="..."
+                            style="width:100%;height:250px;" />
+                            @endif
+                            <div class="course-content">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h4>
+                                        <a href="/category/{{ $artikel->category->slug }}"
+                                            class="artikel">{{ $artikel->category->name }}</a>
+                                    </h4>
+                                    <!-- <p class="price">$169</p> -->
                                 </div>
+                                <h3><a href="/showartikel/{{ $artikel->slug }}">{{ $artikel->title }}</a>
+                                </h3>
+
+                                <p>{{ $artikel->excerpt }}...</p>
+                                <p><a href="/showartikel/{{ $artikel->slug }}">Read More ...</a></p>
                             </div>
                         </div>
                     @empty
--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,6 @@ Route::get('/artikel', function () {
     $article = App\Models\Post::where('status', 'PUBLISHED')->get();
     return view('artikel', compact('settings', 'article'));
 });
-<<<<<<< HEAD
 // Route::get('/profile', function () {
 //     $settings = App\Models\Setting::all();
 //     App\Models\Bgcontent::all();
@@ -70,8 +69,6 @@ Route::get('/artikel', function () {
 Route::get('/kurikulum', 'WebController@kurikulum');
 Route::get('/bkk', 'WebController@bkk');
 Route::get('/bkk/{bkk:slug}', 'WebController@bkkmenu');
-=======
->>>>>>> c5e2aaa9ca2f204c1425a61bfd0c94d5c140055e
 
 Route::get('/kontakkami', function () {
     $settings = App\Models\Setting::all();
