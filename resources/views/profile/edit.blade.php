c3815e910b9274cf91ed02e28ddd60e5c378e0ad
Andhika Rizki <andhikarizki01nougat@gmail.com>
2021-10-20 13:58:19 +0700

    Update edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
-@section('main')        
-        
+@section('main')
+
         <section class="section">
           <div class="section-header">
             <h1>Profile</h1>
@@ -14,13 +14,13 @@
 
           }} --}}
           <div class="section-body">
-            <h2 class="section-title">Hi, {{Str::words($profile->name,1)}}!</h2>
+            <h2 class="section-title">Hai, {{Str::words($profile->name,1)}}!</h2>
             <p class="section-lead">
-              Ubah informasi tentang diri kamu di halaman ini .
+              Ubah informasi tentang diri kamu di halaman ini.
             </p>
 
             <div class="row mt-sm-4">
-  
+
               <div class="col-12 col-md-12 col-lg-12">
                 <center>
 
@@ -28,13 +28,13 @@
                     <div class="card profile-widget">
                       <div class="profile-widget-header">
                         <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" id="outputProfile" class="rounded-circle profile-widget w-50 h-25">
-                        
+
                       </div>
                       <div class="profile-widget-description">
                         <div class="profile-widget-name">{{$profile->name}} <div class="text-muted d-inline font-weight-normal"><div class="slash"></div>{{$profile->kelas->nama_kelas ?? Auth::user()->role->display_name}}</div></div>
-                       
+
                       </div>
-                      
+
                     </div>
                   </div>
                 </center>
@@ -55,7 +55,7 @@
                               {{$message}}
                             </div>
                             @enderror
-                           
+
                           </div>
                           <div class="form-group col-md-6 col-12">
                             <label>Email</label>
@@ -66,7 +66,7 @@
                             </div>
                             @enderror
                           </div>
-                        
+
                         </div>
                         <div class="row">
                           <div class="form-group col-md-6 col-12">
@@ -100,14 +100,14 @@
                           </div>
                         </div>
                         <div class="row">
-                         
-                         
+
+
                           {{-- <div class="form-group col-12">
                             <label>Bio</label>
                             <textarea class="form-control summernote-simple">Ujang maman is a superhero name in <b>Indonesia</b>, especially in my family. He is not a fictional character but an original hero in my family, a hero for his children and for his wife. So, I use the name as a user in this template. Not a tribute, I'm just bored with <b>'John Doe'</b>.</textarea>
                           </div> --}}
                         </div>
-                        
+
                     </div>
                     <div class="card-footer text-right">
                       <a href="{{redirect()->getUrlGenerator()->previous()}}" class="btn btn-warning">Back</a>
